5946b7e46bf7e7a654f362e9c468082011665151
admiswalker <admiswalker@gmail.com>
2021-12-12 21:59:45 +0900

    update install sh

--- cpm/packages/googletest/install_v1.11.0.sh
+++ cpm/packages/googletest/install_v1.11.0.sh
@@ -2,4 +2,9 @@
 
 wget https://github.com/google/googletest/archive/refs/tags/release-1.11.0.zip
 unzip -n release-1.11.0.zip
-cd ./googletest-release-1.11.0; mkdir -p build; cd build; cmake ..; make
+cd ./googletest-release-1.11.0;
+
+mkdir -p build; cd build; cmake ..; make
+
+mv lib/ ../../
+cd ../googletest; mv include/ ../../
--- cpm/packages/sstd/install_v1.0.0.sh
+++ cpm/packages/sstd/install_v1.0.0.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+wget https://github.com/admiswalker/SubStandardLibrary-SSTD-/archive/refs/tags/v1.0.0.zip
+unzip -n v1.0.0.zip
+cd ./SubStandardLibrary-SSTD--1.0.0/sstd;
+make -j;
+mv include/ ../../
+mv lib/ ../../

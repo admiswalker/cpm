d81b76763113c607005f04a113ff66f8b1f25b2c
admiswalker <admiswalker@gmail.com>
2022-01-12 00:26:47 +0900

    wip

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ CPM is a toy package manager for C++.
 
 # Build time
 
-| build target | environment                              | build time |
-| ------------ | ---------------------------------------- | ---------- |
-| gcc-8.4.0    | 8 core, 32 GB, Ubuntu 18.04 LTS (Docker) | 70 mins    |
-| cmake-3.20.1 | 8 core, 32 GB, Ubuntu 20.04 LTS          | 45 mins    |
+| build target | environment                                           | build time     |
+| ------------ | ----------------------------------------------------- | -------------- |
+| sstd-1.0.0   | 8 core, 32 GB / gcc-8.4.0 / Ubuntu 20.04 LTS          |  0 mins 20 sec |
+| gcc-8.4.0    | 8 core, 32 GB / gcc-7.5.0 / Ubuntu 18.04 LTS (Docker) | 70 mins        |
+| cmake-3.20.1 | 8 core, 32 GB / gcc-8.4.0 / Ubuntu 20.04 LTS          | 45 mins        |
 
-Note. Building gcc may require a lot of memory. Using 32 or 64 GB RAM machine and adding a swap memory as insurance.
+Note. Building gcc and cmake may require a lot of memory. Using 32 or 64 GB RAM machine and adding a swap memory as insurance.
 
 
 # File and Directory descriptions
--- cpm/packages/sstd/1.0.0/install.sh
+++ cpm/packages/sstd/1.0.0/install.sh
@@ -1,8 +1,52 @@
 #!/bin/bash
 
-wget https://github.com/admiswalker/SubStandardLibrary-SSTD-/archive/refs/tags/v1.0.0.zip
-unzip -n v1.0.0.zip
-cd ./SubStandardLibrary-SSTD--1.0.0/sstd;
-make -j;
-mv include/ ../../
-mv lib/ ../../
+# Usage
+#   - Set values of `CACHE_DIR`, `BUILD_DIR` and `INST_PATH` using `export` before call this script.
+
+mkdir -p $CACHE_DIR
+mkdir -p $BUILD_DIR
+mkdir -p $INST_PATH
+
+URL=https://github.com/admiswalker/SubStandardLibrary-SSTD-/archive/refs/tags/v1.0.0.zip
+fName=${URL##*/}       # v1.0.0.zip
+fName_base=${fName%.*} # v1.0.0
+fName_ext=${fName#*.}  # zip
+#libName=${fName%-*}   # release
+libName=sstd-1.0.0     # sstd
+
+URL_hash=''
+fName_hash=${URL_hash##*/} # openssl-1.1.1m.tar.gz.sha256
+
+# installing depending packages
+# - gcc-8.4.0 as a base compiler
+# - cmake
+
+# downloading source file
+if [ ! -e $CACHE_DIR/$libName/$fName ]; then
+    wget -P $CACHE_DIR/$libName $URL
+fi
+
+# check hash
+#find $CACHE_DIR/$libName -name $fName_hash -type f -print0 | xargs -0 grep $(sha256sum $CACHE_DIR/$libName/$fName) >/dev/null 2>&1
+echo $(sha256sum $CACHE_DIR/$libName/$fName) | grep 'fd443523f98dadfb60d0bb85d3b4bd26aec68703655944fb96ab4a33b9de5084' >/dev/null 2>&1
+if [ ! $? = 0 ]; then
+    echo 'ERROR: cmake: hash value of downloaded file is not match.'
+    return -1
+fi
+
+# unpacking the archive file
+if [ ! -e $BUILD_DIR/SubStandardLibrary-SSTD--1.0.0 ]; then
+    unzip -n $CACHE_DIR/$libName/$fName -d $BUILD_DIR # zip
+    #tar -zxf $CACHE_DIR/$libName/$fName -C $BUILD_DIR # tar.xz
+fi
+
+# --- installation: begin ---------------------------------------------
+if [ ! -e $INST_PATH/lib/libsstd.a ]; then
+    cd $BUILD_DIR/SubStandardLibrary-SSTD--1.0.0/sstd;
+    
+    make -j
+    
+    cp -r lib/* -t $INST_PATH/lib
+    cp -r include/* -t $INST_PATH/include
+fi
+# ----------------------------------------------- installation: end ---

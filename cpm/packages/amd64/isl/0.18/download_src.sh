2b3ba12f9275ebd1b3fc36c61d4a807e885bf38c
admiswalker <admiswalker@gmail.com>
2022-03-22 20:32:08 +0900

    add isl-0.18 install script

---
+++ cpm/packages/amd64/isl/0.18/download_src.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CPM_CALL_DIR=`pwd -P`
+. $CPM_CALL_DIR/cpm/init_path_and_dir.sh
+. $CPM_OWN_DIR/common_fn.sh
+
+
+URL=https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.18.tar.bz2
+fName=${URL##*/}         # isl-0.18.tar.bz2
+fName_base=${fName%.*.*} # isl-0.18.tar.bz2
+libName='isl'
+ver='0.18'
+
+
+#URL_hash=''
+#fName_hash=${URL_hash##*/} # xxx-x.x.x-sha256sum.txt
+fName_hash='isl-0.18-sha256sum.txt' # xxx-x.x.x-sha256sum.txt
+
+
+cfn_echo_download_begin $libName $ver
+
+
+# downloading source file
+if [ ! -e $CPM_CACHE_DIR/$fName ]; then
+    mkdir -p $CPM_CACHE_DIR
+    cd $CPM_CACHE_DIR; wget -O $fName $URL
+fi
+if [ ! -e $CPM_CACHE_DIR/$fName_hash ]; then
+#    wget -P $CPM_CACHE_DIR $URL_hash
+    echo '6b8b0fd7f81d0a957beb3679c81bbb34ccc7568d5682844d8924424a0dadcb1b  isl-0.18.tar.bz2' > $CPM_CACHE_DIR/$fName_hash
+fi
+cfn_check_hash_value
+
+
+cfn_echo_download_end $libName $ver
---
+++ cpm/packages/amd64/isl/0.18/install_src.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+CPM_CALL_DIR=`pwd -P`
+. $CPM_CALL_DIR/cpm/init_path_and_dir.sh
+. $CPM_OWN_DIR/common_fn.sh
+. $CPM_CALL_DIR/cpm/set_env.sh
+
+
+libName='isl'
+ver='0.18'
+cfn_echo_install_begin $libName $ver
+
+
+fName='isl-0.18.tar.bz2'
+fName_base=${fName%.*.*} # mpfr-3.1.4
+
+# unpacking the archive file
+if [ ! -e $CPM_BUILD_DIR/$fName_base ]; then
+    #unzip -n $CPM_CACHE_DIR/$libName/$fName -d $CPM_BUILD_DIR
+    #tar -zxf $CPM_CACHE_DIR/$fName -C $CPM_BUILD_DIR # tar.xz
+    tar -xvf $CPM_CACHE_DIR/$fName -C $CPM_BUILD_DIR # tar.bz2
+fi
+
+# installation
+if [ `cfn_isInstalled` = 'false' ]; then
+    cd $CPM_BUILD_DIR/$fName_base/
+
+    ./configure --prefix=$CPM_INST_WDIR --with-gmp-prefix=$CPM_DLIB_PATH
+    make -j
+    make check
+    make install
+
+    # gen `replacement_path_for_cpm_archive.txt`
+    echo $CPM_INST_WDIR > $CPM_INST_WDIR/replacement_path_for_cpm_archive.txt
+    
+    cd $CPM_CALL_DIR
+fi
+
+
+cfn_echo_install_end $libName $ver

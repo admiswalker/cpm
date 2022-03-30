c6acf0f350151328a853b84221ac27f72bf218d6
admiswalker <admiswalker@gmail.com>
2022-03-31 01:20:34 +0900

    modify DL script

--- cpm/packages/amd64/gcc/7.5.0/download_archive.sh
+++ cpm/packages/amd64/gcc/7.5.0/download_archive.sh
@@ -7,18 +7,22 @@ CPM_CALL_DIR=`pwd -P`
 
 URL00=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/gcc/7.5.0/amd64-gcc-7.5.0.tar.xz-00
 URL01=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/gcc/7.5.0/amd64-gcc-7.5.0.tar.xz-01
-fName00=${URL00##*/}     # <architecture>-<libName>-<version>.tar.xz-00
-fName01=${URL01##*/}     # <architecture>-<libName>-<version>.tar.xz-01
 
-fName=${fName00%-00}     # <architecture>-<libName>-<version>.tar.xz
+URL_hash=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/gcc/7.5.0/amd64-gcc-7.5.0-sha256sum.txt
+
+# SET: fName, fName_base, arcName, libName, ver
+fName=''
+if [ $URL != 0 ]; then
+    fName=${URL##*/}     # <architecture>-<libName>-<version>.tar.xz
+elif [ $URL00 != 0 ]; then
+    fName00=${URL00##*/} # <architecture>-<libName>-<version>.tar.xz-00
+    fName=${fName00%-00} # <architecture>-<libName>-<version>.tar.xz
+fi
 fName_base=${fName%.*.*} # <architecture>-<libName>-<version>
 tmp=${fName%-*}          # <architecture>-<libName>
 arcName=${tmp%-*}        # <architecture>
 libName=${tmp#*-}        # <libName>
 ver=${fName_base#*-*-}   # <version>
-
-
-URL_hash=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/gcc/7.5.0/amd64-gcc-7.5.0-sha256sum.txt
 fName_hash=${URL_hash##*/} # <architecture>-<libName>-<version>-sha256sum.txt
 
 
@@ -30,21 +34,17 @@ for i in `seq 0 99`
 do
     num=`printf "%02d\n" $i`
     
-    URLxx=URL$num
-    cmd_u='echo $'$URLxx
+    URL_xx=URL$num
+    cmd_u='echo $'$URL_xx
     URL_str=`eval $cmd_u`
     URL_len=${#URL_str}
-    
-    fNamexx=fName$num
-    cmd_f='echo $'$fNamexx
-    fName_str=`eval $cmd_u`
-    fName_len=${#fName_str}
-    
+
     if [ $URL_len == 0 ]; then
 	break
     fi
     
-    if [ ! -e $CPM_CACHE_DIR/$fName_str ]; then
+    fName_xx=${URL_str##*/} # <architecture>-<libName>-<version>.tar.xz-xx
+    if [ ! -e $CPM_CACHE_DIR/$fName_xx ]; then
 	wget -P $CPM_CACHE_DIR $URL_str
     fi
 done

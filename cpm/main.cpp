aada1dc6882e353392e777cf35d7a7e37d20114f
admiswalker <admiswalker@gmail.com>
2022-04-01 22:58:28 +0900

    add path-fn

--- cpm/main.cpp
+++ cpm/main.cpp
@@ -145,9 +145,9 @@ bool read_packages_dir(std::unordered_map<std::string,std::vector<struct pkg>>& 
     
     return true;
 }
-std::vector<struct pkg> solve_depends___dummy(bool& ret,
-                           const std::vector<struct pkg>& v_pkg_requested,
-                           const std::unordered_map<std::string, std::vector<struct pkg>>& table_vPkg){
+std::vector<struct pkg> solve_dependencies(bool& ret,
+                                           const std::vector<struct pkg>& v_pkg_requested,
+                                           const std::unordered_map<std::string, std::vector<struct pkg>>& table_vPkg){
     ret=true;
     std::vector<struct pkg> v_pkg_ret;
     
@@ -191,16 +191,29 @@ void gen_archive(const std::string& save_name, const std::string& archive_ext, c
     }
     return;
 }
-void gen_hashFile(const std::string& archive_dir, const std::string& save_name){
+void gen_hashFile(const std::string& ACV_DIR, const std::string& save_name){
     std::string w_str;
     
-    std::vector<std::string> vPath = sstd::glob(archive_dir+R"(/*)");
+    std::vector<std::string> vPath = sstd::glob(ACV_DIR+R"(/*)");
     for(uint i=0; i<vPath.size(); ++i){
-        w_str += sstd::system_stdout("cd "+archive_dir+"; sha256sum "+&vPath[i][archive_dir.size()+1]);
+        w_str += sstd::system_stdout("cd "+ACV_DIR+"; sha256sum "+&vPath[i][ACV_DIR.size()+1]);
     }
     sstd::write(save_name, w_str);
     return;
 }
+std::string getPath_acvPkgDir      (const std::string& ACV_DIR,   const std::string& architecture, const struct pkg& p){ return ACV_DIR + '/' + architecture + '/' + p.name + '/' + p.ver; }
+std::string getPath_acvBaseName    (const std::string& ACV_DIR,   const std::string& architecture, const struct pkg& p){ return getPath_acvPkgDir(ACV_DIR, architecture, p) + '/' + architecture + '-' + p.name + '-' + p.ver; }
+std::string getPath_packsPkgDir    (const std::string& PACKS_DIR, const struct pkg& p){ return PACKS_DIR + '/' + p.name + '/' + p.ver; }
+std::string getPath_cachePkgDir_acv(const std::string& CACHE_DIR, const std::string& architecture, const struct pkg& p){ return CACHE_DIR + "/archive/" + architecture + '-' + p.name + '-' + p.ver; }
+std::string getPath_cachePkgDir_src(const std::string& CACHE_DIR, const std::string& architecture, const struct pkg& p){ return CACHE_DIR + "/src/"     + architecture + '-' + p.name + '-' + p.ver; }
+std::string getPath_buildPkgDir    (const std::string& BUILD_DIR, const std::string& architecture, const struct pkg& p){ return BUILD_DIR + '/' + architecture + '-' + p.name + '-' + p.ver; }
+
+std::string getSh_dlAcv  (const std::string& packsPkg_dir){ return packsPkg_dir + "/download_archive.sh"; }
+std::string getSh_dlSrc  (const std::string& packsPkg_dir){ return packsPkg_dir + "/download_src.sh";     }
+std::string getSh_instAcv(const std::string& packsPkg_dir){ return packsPkg_dir + "/install_archive.sh";  }
+std::string getSh_instSrc(const std::string& packsPkg_dir){ return packsPkg_dir + "/install_src.sh";      }
+std::string getSh_isInst (const std::string& packsPkg_dir){ return packsPkg_dir + "/is_installed.sh";     }
+
 bool install_libs(const std::string& CACHE_DIR,
                   const std::string& BUILD_DIR,
                   const std::string& INST_PATH,
@@ -209,7 +222,7 @@ bool install_libs(const std::string& CACHE_DIR,
                   const std::string& architecture,
                   const std::string& PACKS_DIR,
                   const std::vector<struct pkg>& v_pkg,
-                  const bool TF_genArchive, const std::string& archive_dir, const std::string& archive_ext,
+                  const bool TF_genArchive, const std::string& ACV_DIR, const std::string& archive_ext,
                   const bool TF_dl_dps2cache_only,
                   const std::string& install_mode)
 {
@@ -218,22 +231,22 @@ bool install_libs(const std::string& CACHE_DIR,
     for(uint i=0; i<v_pkg.size(); ++i){
         struct pkg p = v_pkg[i];
         
-        std::string archive_pkg_dir;
-        std::string archive_path;
+        std::string archivePkg_dir;
+        std::string archive_baseName;
         if(TF_genArchive){
-            archive_pkg_dir = archive_dir + '/' + architecture + '/' + p.name + '/' + p.ver;
-            archive_path = archive_pkg_dir + '/' + architecture + '-' + p.name + '-' + p.ver;
-            if(sstd::fileExist(archive_path+'.'+archive_ext)){ continue; }
+            archivePkg_dir   = getPath_acvPkgDir  (ACV_DIR, architecture, p);
+            archive_baseName = getPath_acvBaseName(ACV_DIR, architecture, p);
+            if(sstd::fileExist(archive_baseName+'.'+archive_ext)){ continue; }
         }
         
-        std::string pkg_shell_dir = PACKS_DIR + '/' + p.name + '/' + p.ver;
-        bool TF_useArchive = sstd::isFile(pkg_shell_dir + "/download_archive.sh") && (install_mode != cmd_INSTALL_MODE_source);
+        std::string packsPkg_dir = getPath_packsPkgDir(PACKS_DIR, p);
+        bool TF_useArchive = sstd::isFile(getSh_dlAcv(packsPkg_dir)) && (install_mode != cmd_INSTALL_MODE_source);
         
-        std::string cacheDir_pkg = CACHE_DIR + '/' + (TF_useArchive ? "archive":"src") + '/' + architecture + '-' + p.name + '-' + p.ver;
-        std::string buildDir_pkg = BUILD_DIR + '/' + architecture + '-' + p.name + '-' + p.ver;
-        sstd::mkdir(cacheDir_pkg);
+        std::string cachePkg_dir = (TF_useArchive ? getPath_cachePkgDir_acv(CACHE_DIR, architecture, p):getPath_cachePkgDir_src(CACHE_DIR, architecture, p));
+        std::string buildPkg_dir = getPath_buildPkgDir(BUILD_DIR, architecture, p);
+        sstd::mkdir(cachePkg_dir);
         if(!TF_useArchive){
-            sstd::mkdir(buildDir_pkg);
+            sstd::mkdir(buildPkg_dir);
         }
         
         std::string cmd_env;
@@ -248,33 +261,33 @@ bool install_libs(const std::string& CACHE_DIR,
             sstd::pdbg("ERROR: BUILD_ENV has invalid value: %s.\n", v_build_env[0].c_str());
             return false;
         }
-        cmd_env += "export CPM_BUILD_DIR=" + buildDir_pkg + '\n';
+        cmd_env += "export CPM_BUILD_DIR=" + buildPkg_dir + '\n';
         cmd_env += "export CPM_DLIB_PATH=" + INST_PATH + '\n'; // dependent library
         cmd_env += "export CPM_INST_WDIR=" + INST_WDIR + '\n'; // working dir
         cmd_env += "export CPM_INST_PATH=" + INST_PATH + '\n';
         cmd_env += "\n";
         std::string cmd_run;
         if(TF_dl_dps2cache_only){
-            std::string cacheDir_acv = CACHE_DIR + "/archive/" + architecture + '-' + p.name + '-' + p.ver;
-            std::string cacheDir_src = CACHE_DIR + "/src/"     + architecture + '-' + p.name + '-' + p.ver;
+            std::string cacheDir_acv = getPath_cachePkgDir_acv(CACHE_DIR, architecture, p);
+            std::string cacheDir_src = getPath_cachePkgDir_src(CACHE_DIR, architecture, p);
             std::string cmd_env4acv = cmd_env + "export CPM_CACHE_DIR=" + cacheDir_acv + '\n';
             std::string cmd_env4src = cmd_env + "export CPM_CACHE_DIR=" + cacheDir_src + '\n';
-            std::string cmd_run4acv = runner + ' ' + pkg_shell_dir + "/download_archive.sh" + ' ' + options + '\n';
-            std::string cmd_run4src = runner + ' ' + pkg_shell_dir + "/download_src.sh" + ' ' + options + '\n';
+            std::string cmd_run4acv = runner + ' ' + getSh_dlAcv(packsPkg_dir) + ' ' + options + '\n';
+            std::string cmd_run4src = runner + ' ' + getSh_dlSrc(packsPkg_dir) + ' ' + options + '\n';
             sstd::system(cmd_env4acv + cmd_run4acv); // download archive files
             sstd::system(cmd_env4src + cmd_run4src); // download src files
             continue;
         }
-        cmd_env += "export CPM_CACHE_DIR=" + cacheDir_pkg + '\n';
+        cmd_env += "export CPM_CACHE_DIR=" + cachePkg_dir + '\n';
         if(TF_useArchive){
-            cmd_run += runner + ' ' + pkg_shell_dir + "/download_archive.sh" + ' ' + options + '\n';
-            cmd_run += runner + ' ' + pkg_shell_dir + "/install_archive.sh" + ' ' + options + '\n';
+            cmd_run += runner + ' ' + getSh_dlAcv  (packsPkg_dir) + ' ' + options + '\n';
+            cmd_run += runner + ' ' + getSh_instAcv(packsPkg_dir) + ' ' + options + '\n';
         }else{
-            cmd_run += runner + ' ' + pkg_shell_dir + "/download_src.sh" + ' ' + options + '\n';
-            cmd_run += runner + ' ' + pkg_shell_dir + "/install_src.sh" + ' ' + options + '\n';
+            cmd_run += runner + ' ' + getSh_dlSrc  (packsPkg_dir) + ' ' + options + '\n';
+            cmd_run += runner + ' ' + getSh_instSrc(packsPkg_dir) + ' ' + options + '\n';
         }
-        //std::string str_isInstalled = sstd::system_stdout_stderr(cmd_env+runner+' '+pkg_shell_dir+"/is_installed.sh"+' '+options+'\n');
-        std::string str_isInstalled = sstd::system_stdout_stderr(cmd_env+pkg_shell_dir+"/is_installed.sh");
+        //std::string str_isInstalled = sstd::system_stdout_stderr(cmd_env+runner+' '+getSh_isInst(packsPkg_dir)+' '+options+'\n');
+        std::string str_isInstalled = sstd::system_stdout_stderr(cmd_env + getSh_isInst(packsPkg_dir));
         bool TF_isInstalled = sstd::strIn("true", str_isInstalled);
         if(TF_isInstalled){ continue; }
         sstd::mkdir(INST_WDIR);
@@ -305,7 +318,7 @@ bool install_libs(const std::string& CACHE_DIR,
             return false;
         }
         
-        TF_isInstalled = sstd::stripAll(sstd::system_stdout(cmd_env+pkg_shell_dir+"/is_installed.sh"), " \r\n")=="true";
+        TF_isInstalled = sstd::stripAll(sstd::system_stdout(cmd_env + getSh_isInst(packsPkg_dir)), " \r\n")=="true";
         if(!TF_isInstalled){
             sstd::pdbg("ERROR: Installation is failed.");
             return false;
@@ -316,9 +329,9 @@ bool install_libs(const std::string& CACHE_DIR,
                 return false;
             }
             
-            sstd::mkdir(archive_pkg_dir);
-            gen_archive(call_path+'/'+archive_path+'.'+archive_ext, archive_ext, call_path+'/'+INST_WDIR);
-            gen_hashFile(archive_pkg_dir, archive_path+"-sha256sum.txt");
+            sstd::mkdir(archivePkg_dir);
+            gen_archive(call_path+'/'+archive_baseName+'.'+archive_ext, archive_ext, call_path+'/'+INST_WDIR);
+            gen_hashFile(archivePkg_dir, archive_baseName+"-sha256sum.txt");
         }
         sstd::rm(INST_WDIR);
     }
@@ -389,12 +402,12 @@ int main(int argc, char *argv[]){
     
     // Because of when using Docker, the absolute path is determined at run time.
     // Use relative path internally and convert absolute path when immediately before running scripts and commands.
-    std::string CACHE_DIR   = base_dir+"/cache";
-    std::string PACKS_DIR   = base_dir+"/packages";
-    std::string BUILD_DIR   = base_dir+"/build";
-    std::string INST_WDIR   = base_dir+"/local_work";
-    std::string INST_PATH   = base_dir+"/local";
-    std::string archive_dir = base_dir+"/archive";
+    std::string CACHE_DIR = base_dir+"/cache";
+    std::string PACKS_DIR = base_dir+"/packages";
+    std::string BUILD_DIR = base_dir+"/build";
+    std::string INST_WDIR = base_dir+"/local_work";
+    std::string INST_PATH = base_dir+"/local";
+    std::string ACV_DIR   = base_dir+"/archive";
     sstd::mkdir(CACHE_DIR);
     sstd::mkdir(PACKS_DIR);
     sstd::mkdir(BUILD_DIR);
@@ -440,31 +453,28 @@ int main(int argc, char *argv[]){
             std::string libName = vCmd[1];
             std::string ver     = vCmd[2];
             std::string URL     = sstd::stripAll(vCmd[3], "\"");
-            std::string dl_script_cache_path = CACHE_DIR+"/import/"+libName+'/'+ver;
-            std::string cache_pkg_dir        = CACHE_DIR+"/packages/"+libName+'/'+ver;
-            std::string pkg_shell_dir        = PACKS_DIR + '/' + architecture + '/' + libName + '/' + ver;
-            sstd::mkdir(dl_script_cache_path);
-            sstd::mkdir(cache_pkg_dir);
-            sstd::mkdir(pkg_shell_dir);
-            std::string dl_script = dl_script_cache_path + '/' + sstd::getFileName( URL.c_str() );
+            std::string cacheIptScr_dir = CACHE_DIR+"/import/"+libName+'/'+ver;
+            std::string cachePkg_dir    = CACHE_DIR+"/packages/"+libName+'/'+ver;
+            std::string packsPkg_dir    = PACKS_DIR + '/' + architecture + '/' + libName + '/' + ver;
+            sstd::mkdir(cacheIptScr_dir);
+            sstd::mkdir(cachePkg_dir);
+            sstd::mkdir(packsPkg_dir);
+            std::string dl_script = cacheIptScr_dir + '/' + sstd::getFileName( URL.c_str() );
             if(!sstd::fileExist(dl_script)){
-                sstd::system("wget -P "+dl_script_cache_path+' '+URL);
+                sstd::system("wget -P "+cacheIptScr_dir+' '+URL);
             }
             
             std::string cmd;
-            cmd += "export CPM_CACHE_DIR=" + cache_pkg_dir + '\n';
+            cmd += "export CPM_CACHE_DIR=" + cachePkg_dir + '\n';
             cmd += "./" + dl_script;
             sstd::system(cmd);
-            sstd::cp(cache_pkg_dir+"/*", pkg_shell_dir, "pu");
+            sstd::cp(cachePkg_dir+"/*", packsPkg_dir, "pu");
             
             // add pkg to table_vPkg
             struct pkg r; if(!str2struct_pkg(r, libName, ver)){ return false; }
             table_vPkg[ r.name ] <<= r;
         }else if(vCmd[0]==cmd_INSTALL_MODE){
-            if(vCmd.size()<2){
-                sstd::pdbg("ERROR: INSTALL_MODE is empty.");
-                return -1;
-            }
+            if(vCmd.size()<2){ sstd::pdbg("ERROR: INSTALL_MODE is empty."); return -1; }
             
             if      (vCmd[1]==cmd_INSTALL_MODE_auto   ){ install_mode = cmd_INSTALL_MODE_auto;
             }else if(vCmd[1]==cmd_INSTALL_MODE_source ){ install_mode = cmd_INSTALL_MODE_source;
@@ -482,8 +492,8 @@ int main(int argc, char *argv[]){
         
         bool ret=true;
         std::vector<struct pkg> v_pkg_requested = vPkgList2vPkg(ret, vPkgList); if(!ret){ return -1; }
-        std::vector<struct pkg> v_inst_pkg      = solve_depends___dummy(ret, v_pkg_requested, table_vPkg); if(!ret){ return -1; }
-        install_libs(CACHE_DIR, BUILD_DIR, INST_PATH, INST_WDIR, v_build_env, architecture, PACKS_DIR+'/'+architecture, v_inst_pkg, TF_genArchive, archive_dir, archive_ext, TF_dl_dps2cache_only, install_mode);
+        std::vector<struct pkg> v_inst_pkg      = solve_dependencies(ret, v_pkg_requested, table_vPkg); if(!ret){ return -1; }
+        install_libs(CACHE_DIR, BUILD_DIR, INST_PATH, INST_WDIR, v_build_env, architecture, PACKS_DIR+'/'+architecture, v_inst_pkg, TF_genArchive, ACV_DIR, archive_ext, TF_dl_dps2cache_only, install_mode);
     }
 
     printf("\n");

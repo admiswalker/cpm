22639dcd9df8892efd1cc79705e29e299d1463d5
admiswalker <admiswalker@gmail.com>
2022-04-02 09:16:01 +0900

    wip

--- cpm/src/version_processor.hpp
+++ cpm/src/version_processor.hpp
@@ -1,7 +1,28 @@
 #pragma once
+#include <sstd/sstd.hpp>
 
 
 namespace cpm{
     int plus(int, int);
+    
+    struct verRange{
+        std::string L;
+        uint LIS; // left inequality-sign
+        uint RIS; // right inequality-sign
+        std::string R;
+    };
+
+    struct verRange str2verStruct(const std::string& verStr);
 }
+
+// range: L, R
+// - '-inf'
+// - '0.0.0' ~ '*.*.*'
+// - 'inf'
+
+// inequality-sign
+// - ASCII Code
+//   - <: 92
+//   - =: 93
+//   - >: 94
 
--- cpm/test/version_processor.hpp
+++ cpm/test/version_processor.hpp
@@ -1,6 +1,43 @@
 #pragma once
+#include "../src/version_processor.hpp"
+
+
+//sstd, 0.0.0<=ver<=2.0.1;
+
+//sstd, ver<=2.0.1;
+//sstd, 2.0.1>=ver>-inf;
+
+//sstd, 2.0.1;
+//sstd, ver==2.0.1;
+//sstd, 2.0.1==ver;
+
+//sstd, 2.0.1<=ver;
+//sstd, 2.0.1<=ver<inf;
+//sstd,     ver>=2.0.1;
+//sstd, inf>ver>=2.0.1;
 
 
 TEST(version_processor, v01){
+    ASSERT_EQ(1, 1);
+}
+TEST(version_processor, case01){
+    std::string v = "0.0.0<=ver<=2.0.1";
+    
+    struct cpm::verRange vr = cpm::str2verStruct( v );
+    
+    ASSERT_EQ(1, 1);
+}
+TEST(version_processor, case02){
+    std::string v = "<=0.0.0<=ver<=2.0.1";
+    
+    struct cpm::verRange vr = cpm::str2verStruct( v );
+    
+    ASSERT_EQ(1, 1);
+}
+TEST(version_processor, case03){
+    std::string v = "0.0.0<=ver<=2.0.1<=";
+    
+    struct cpm::verRange vr = cpm::str2verStruct( v );
+    
     ASSERT_EQ(1, 1);
 }

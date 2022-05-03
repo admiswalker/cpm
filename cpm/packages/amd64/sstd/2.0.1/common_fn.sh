b89ebd81e95a0bbc5ff49130480640369afdaaae
admiswalker <admiswalker@gmail.com>
2022-05-04 00:50:40 +0900

    fix indent

--- cpm/packages/amd64/sstd/2.0.1/common_fn.sh
+++ cpm/packages/amd64/sstd/2.0.1/common_fn.sh
@@ -5,13 +5,13 @@ cfn_download_archive(){
     # SET: fName, fName_base, arcName, libName, ver
     fName=''
     if [ ${#URL} != 0 ]; then
-	fName=${URL##*/}       # <architecture>-<libName>-<version>.tar.xz
+        fName=${URL##*/}       # <architecture>-<libName>-<version>.tar.xz
     elif [ ${#URL00} != 0 ]; then
-	fName00=${URL00##*/}   # <architecture>-<libName>-<version>.tar.xz-00
-	fName=${fName00%-00}   # <architecture>-<libName>-<version>.tar.xz
+        fName00=${URL00##*/}   # <architecture>-<libName>-<version>.tar.xz-00
+        fName=${fName00%-00}   # <architecture>-<libName>-<version>.tar.xz
     else
-	echo -e "\e[31mERROR: unexpected URL pattern.\e[m"
-	exit -1
+        echo -e "\e[31mERROR: unexpected URL pattern.\e[m"
+        exit -1
     fi
     fName_base=${fName%.*.*}   # <architecture>-<libName>-<version>
     tmp=${fName%-*}            # <architecture>-<libName>
@@ -23,54 +23,54 @@ cfn_download_archive(){
 
     # downloading archive file
     if [ ${#URL} != 0 ]; then
-	# downloading single-archive file
-	if [ ! -e $CPM_CACHE_DIR/$fName ]; then
-	    wget -P $CPM_CACHE_DIR $URL
-	fi
+        # downloading single-archive file
+        if [ ! -e $CPM_CACHE_DIR/$fName ]; then
+            wget -P $CPM_CACHE_DIR $URL
+        fi
     else
-	# downloading multiple-archive file
-	for i in `seq 0 99`
-	do
-	    num=`printf "%02d\n" $i`
-	    
-	    URL_xx=URL$num
-	    cmd_u='echo $'$URL_xx
-	    URL_str=`eval $cmd_u`
-	    URL_len=${#URL_str}
+        # downloading multiple-archive file
+        for i in `seq 0 99`
+        do
+            num=`printf "%02d\n" $i`
+            
+            URL_xx=URL$num
+            cmd_u='echo $'$URL_xx
+            URL_str=`eval $cmd_u`
+            URL_len=${#URL_str}
 
-	    if [ $URL_len == 0 ]; then
-		break
-	    fi
-	    
-	    fName_xx=${URL_str##*/} # <architecture>-<libName>-<version>.tar.xz-xx
-	    if [ ! -e $CPM_CACHE_DIR/$fName_xx ]; then
-		wget -P $CPM_CACHE_DIR $URL_str
-	    fi
-	done
-	if [ ! -e $CPM_CACHE_DIR/$fName ]; then
-	    cat $CPM_CACHE_DIR/$fName-* >> $CPM_CACHE_DIR/$fName
-	fi
+            if [ $URL_len == 0 ]; then
+                break
+            fi
+            
+            fName_xx=${URL_str##*/} # <architecture>-<libName>-<version>.tar.xz-xx
+            if [ ! -e $CPM_CACHE_DIR/$fName_xx ]; then
+                wget -P $CPM_CACHE_DIR $URL_str
+            fi
+        done
+        if [ ! -e $CPM_CACHE_DIR/$fName ]; then
+            cat $CPM_CACHE_DIR/$fName-* >> $CPM_CACHE_DIR/$fName
+        fi
     fi
     if [ ! -e $CPM_CACHE_DIR/$fName_hash ]; then
-	wget -P $CPM_CACHE_DIR $URL_hash
+        wget -P $CPM_CACHE_DIR $URL_hash
     fi
     cfn_check_hash_value
 }
 cfn_check_hash_value(){
     if ([[ $fName_hash == *"sha"* ]] || [[ $fName_hash == *"SHA"* ]]) && [[ $fName_hash == *"256"* ]]; then
-	find $CPM_CACHE_DIR -name $fName_hash -type f -print0 | xargs -0 grep $(sha256sum $CPM_CACHE_DIR/$fName) >/dev/null 2>&1
-	if [ ! $? = 0 ]; then
-	    echo -e "\e[31mERROR: hash value of downloaded file is not match.\e[m"
-	    exit -1
-	fi
+        find $CPM_CACHE_DIR -name $fName_hash -type f -print0 | xargs -0 grep $(sha256sum $CPM_CACHE_DIR/$fName) >/dev/null 2>&1
+        if [ ! $? = 0 ]; then
+            echo -e "\e[31mERROR: hash value of downloaded file is not match.\e[m"
+            exit -1
+        fi
     elif ([[ $fName_hash == *"sha"* ]] || [[ $fName_hash == *"SHA"* ]]) && [[ $fName_hash == *"512"* ]]; then
-	find $CPM_CACHE_DIR -name $fName_hash -type f -print0 | xargs -0 grep $(sha512sum $CPM_CACHE_DIR/$fName) >/dev/null 2>&1
-	if [ ! $? = 0 ]; then
-	    echo -e "\e[31mERROR: hash value of downloaded file is not match.\e[m"
-	    exit -1
-	fi
+        find $CPM_CACHE_DIR -name $fName_hash -type f -print0 | xargs -0 grep $(sha512sum $CPM_CACHE_DIR/$fName) >/dev/null 2>&1
+        if [ ! $? = 0 ]; then
+            echo -e "\e[31mERROR: hash value of downloaded file is not match.\e[m"
+            exit -1
+        fi
     else
-	echo -e "\e[31mERROR: invalid hash file name.\e[m"
+        echo -e "\e[31mERROR: invalid hash file name.\e[m"
     fi
 }
 cfn_install_archive(){
@@ -82,6 +82,6 @@ cfn_install_archive(){
 
     # installation
     if [ `cfn_isInstalled` = 'false' ]; then
-	tar -Jxf $CPM_CACHE_DIR/$fName -C $CPM_INST_WDIR # tar.xz
+        tar -Jxf $CPM_CACHE_DIR/$fName -C $CPM_INST_WDIR # tar.xz
     fi
 }

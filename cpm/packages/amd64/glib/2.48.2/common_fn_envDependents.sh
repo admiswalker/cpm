5fbbc54000107be0fdda1df2c2fd09964a457703
admiswalker <admiswalker@gmail.com>
2022-06-04 02:59:57 +0900

    fix glib-2.48.2

--- cpm/packages/amd64/glib/2.48.2/common_fn_envDependents.sh
+++ cpm/packages/amd64/glib/2.48.2/common_fn_envDependents.sh
@@ -2,9 +2,9 @@
 
 
 cfn_set_archive_URL(){
-    URL=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/glib/2.31.8/amd64-glib-2.48.2.tar.xz
+    URL=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/glib/2.48.2/amd64-glib-2.48.2.tar.xz
     
-    URL_hash=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/glib/2.31.8/amd64-glib-2.48.2-sha256sum.txt
+    URL_hash=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/glib/2.48.2/amd64-glib-2.48.2-sha256sum.txt
 }
 cfn_set_archive_fName(){
     fName=amd64-glib-2.48.2.tar.xz

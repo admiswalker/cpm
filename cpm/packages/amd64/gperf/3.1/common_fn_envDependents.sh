cbc60e95e75ae678d7f26368265f45c217908ae7
admiswalker <admiswalker@gmail.com>
2022-06-04 16:49:34 +0900

    add gperf/3.1

---
+++ cpm/packages/amd64/gperf/3.1/common_fn_envDependents.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+cfn_set_archive_URL(){
+    URL=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/gperf/3.1/amd64-gperf-3.1.tar.xz
+    
+    URL_hash=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/gperf/3.1/amd64-gperf-3.1-sha256sum.txt
+}
+cfn_set_archive_fName(){
+    fName=amd64-gperf-3.1.tar.xz
+}
+cfn_isInstalled(){
+    if [ -e $CPM_INST_PATH/bin/gperf ]; then
+        echo 'true'
+    else
+        echo 'false'
+    fi
+}

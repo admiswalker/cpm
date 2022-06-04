6a06142bb6394f7c994b86b2072115b766daa713
admiswalker <admiswalker@gmail.com>
2022-06-04 16:31:41 +0900

    add amd64/cairo/1.16.0 and amd64/cairo/1.17.4

---
+++ cpm/packages/amd64/cairo/1.16.0/install_src.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+CPM_OWN_DIR=`dirname $0`
+. $CPM_OWN_DIR/init_path.sh
+. $CPM_OWN_DIR/common_fn.sh
+. $CPM_OWN_DIR/common_fn_envDependents.sh
+CPM_CALL_DIR=`pwd -P`
+. $CPM_INST_PATH/set_env.sh
+
+
+fName='cairo-1.16.0.tar.xz'
+fName_base=${fName%.*.*} # <libName>-<version>
+libName=${fName_base%-*} # <libName>
+ver=${fName_base#*-}     # <version>
+
+
+# unpacking the archive file
+if [ ! -e $CPM_BUILD_DIR/$fName_base ]; then
+    #unzip -nq $CPM_CACHE_DIR/$fName -d $CPM_BUILD_DIR
+    tar -xf $CPM_CACHE_DIR/$fName -C $CPM_BUILD_DIR # tar.xz
+    #tar -xvf $CPM_CACHE_DIR/$fName -C $CPM_BUILD_DIR # tar.bz2
+    #tar -zxvf $CPM_CACHE_DIR/$fName -C $CPM_BUILD_DIR # tar.gz
+fi
+
+# installation
+if [ `cfn_isInstalled` = 'false' ]; then
+    cd $CPM_BUILD_DIR/$fName_base/
+
+    export PKG_CONFIG_PATH=$CPM_INST_PATH/lib/pkgconfig
+    ./configure --prefix=$CPM_INST_WDIR
+    
+    make -j
+    make install
+    
+    # gen `replacement_path_for_cpm_archive.txt`
+    echo $CPM_INST_WDIR > $CPM_INST_WDIR/replacement_path_for_cpm_archive.txt
+    
+    cd $CPM_CALL_DIR
+fi
---
+++ cpm/packages/amd64/cairo/1.17.4/install_archive.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CPM_OWN_DIR=`dirname $0`
+. $CPM_OWN_DIR/init_path.sh
+. $CPM_OWN_DIR/common_fn.sh
+. $CPM_OWN_DIR/common_fn_envDependents.sh
+
+
+cfn_set_archive_fName
+
+
+cfn_install_archive

a8d1279f5da836f3343f2cc77a4e56272f895f8f
admiswalker <admiswalker@gmail.com>
2022-06-04 18:38:16 +0900

    fix libtiff

--- cpm/packages/amd64/libtiff/4.4.0rc1/common_fn_envDependents.sh
+++ cpm/packages/amd64/libtiff/4.4.0rc1/common_fn_envDependents.sh
@@ -2,12 +2,12 @@
 
 
 cfn_set_archive_URL(){
-    URL=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/libpng/1.6.37/amd64-libpng-1.6.37.tar.xz
+    URL=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/libtiff/4.4.0rc1/amd64-libtiff-4.4.0rc1.tar.xz
     
-    URL_hash=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/libpng/1.6.37/amd64-libpng-1.6.37-sha256sum.txt
+    URL_hash=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/libtiff/4.4.0rc1/amd64-libtiff-4.4.0rc1-sha256sum.txt
 }
 cfn_set_archive_fName(){
-    fName=amd64-libpng-1.6.37.tar.xz
+    fName=amd64-libtiff-4.4.0rc1.tar.xz
 }
 cfn_isInstalled(){
     if [ -e $CPM_INST_PATH/lib/libtiff.a ]; then

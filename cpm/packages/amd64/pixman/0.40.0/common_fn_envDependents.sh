c7c05a9837e048e0f900fac500b8c452f17a4225
admiswalker <admiswalker@gmail.com>
2022-06-04 16:48:29 +0900

    add pixman/0.40.0

---
+++ cpm/packages/amd64/pixman/0.40.0/common_fn_envDependents.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+cfn_set_archive_URL(){
+    URL=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/pixman/0.40.0/amd64-pixman-0.40.0.tar.xz
+    
+    URL_hash=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/pixman/0.40.0/amd64-pixman-0.40.0-sha256sum.txt
+}
+cfn_set_archive_fName(){
+    fName=amd64-pixman-0.40.0.tar.xz
+}
+cfn_isInstalled(){
+    if [ -e $CPM_INST_PATH/lib/libpixman-1.a ]; then
+        echo 'true'
+    else
+        echo 'false'
+    fi
+}

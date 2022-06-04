0c10309cf81d08cfdbca5e6a6ef46dd5689ab9c0
admiswalker <admiswalker@gmail.com>
2022-06-04 12:39:41 +0900

    add zlib/1.2.12

---
+++ cpm/packages/amd64/zlib/1.2.12/download_src.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+CPM_OWN_DIR=`dirname $0`
+. $CPM_OWN_DIR/init_path.sh
+. $CPM_OWN_DIR/common_fn.sh
+. $CPM_OWN_DIR/common_fn_envDependents.sh
+
+
+URL=http://www.zlib.net/zlib-1.2.12.tar.xz
+fName=${URL##*/}         # <libName>-<version>.tar.xz
+fName_base=${fName%.*.*} # <libName>-<version>
+libName=${fName_base%-*} # <libName>
+ver=${fName_base#*-}     # <version>
+
+
+#URL_hash=''
+#fName_hash=${URL_hash##*/} # sha512.sum
+fName_hash=sha256.sum
+
+
+# downloading source file
+if [ ! -e $CPM_CACHE_DIR/$fName ]; then
+    mkdir -p $CPM_CACHE_DIR
+    cd $CPM_CACHE_DIR; wget -O $fName $URL
+fi
+
+if [ ! -e $CPM_CACHE_DIR/$fName_hash ]; then
+#    wget -P $CPM_CACHE_DIR $URL_hash
+    echo '7db46b8d7726232a621befaab4a1c870f00a90805511c0e0090441dac57def18  zlib-1.2.12.tar.xz' > $CPM_CACHE_DIR/$fName_hash
+fi
+cfn_check_hash_value

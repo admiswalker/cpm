80b81e3adcaec0b003d7bc711fc5e70580e18c41
admiswalker <admiswalker@gmail.com>
2022-06-11 14:11:36 +0900

    fix glib/2.73.0

--- cpm/packages/amd64/glib/2.73.0/common_fn_envDependents.sh
+++ cpm/packages/amd64/glib/2.73.0/common_fn_envDependents.sh
@@ -2,12 +2,12 @@
 
 
 cfn_set_archive_URL(){
-    URL=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/glib/2.48.2/amd64-glib-2.48.2.tar.xz
+    URL=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/glib/2.73.0/amd64-glib-2.73.0.tar.xz
     
-    URL_hash=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/glib/2.48.2/amd64-glib-2.48.2-sha256sum.txt
+    URL_hash=https://github.com/admiswalker/cpm_archive/raw/main/archive/amd64/glib/2.73.0/amd64-glib-2.73.0-sha256sum.txt
 }
 cfn_set_archive_fName(){
-    fName=amd64-glib-2.48.2.tar.xz
+    fName=amd64-glib-2.73.0.tar.xz
 }
 cfn_isInstalled(){
     if [ -e $CPM_INST_PATH/lib/x86_64-linux-gnu/libgio-2.0.so ]; then

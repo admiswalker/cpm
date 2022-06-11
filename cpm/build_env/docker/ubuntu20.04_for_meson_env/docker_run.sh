bb8e35c5a008e897b2364001834a8cffc3427ae4
admiswalker <admiswalker@gmail.com>
2022-06-11 14:39:31 +0900

    add uid and gid to docker_run

--- cpm/build_env/docker/ubuntu20.04_for_meson_env/docker_run.sh
+++ cpm/build_env/docker/ubuntu20.04_for_meson_env/docker_run.sh
@@ -10,4 +10,4 @@ CMD=$1
 DOCKER_OPTIONS=${ALL_ARG#$CMD}
 SCRIPT_DIR=`cd $(dirname ${BASH_SOURCE:-$0}); pwd`
 DOCKER_BASE_PATH=`cat $SCRIPT_DIR/docker_base_path.txt | tr -d '\r' | tr -d '\n'`
-docker run --rm -it --name run_cpm_u22_meson_env -v $PWD:$DOCKER_BASE_PATH -w $DOCKER_BASE_PATH -p 8000:8000 $DOCKER_OPTIONS docker_img_cpm_meson_env:latest bash $CMD
+docker run --rm -it -u $(id -u):$(id -g) --name run_cpm_u22_meson_env -v $PWD:$DOCKER_BASE_PATH -w $DOCKER_BASE_PATH -p 8000:8000 $DOCKER_OPTIONS docker_img_cpm_meson_env:latest bash $CMD
